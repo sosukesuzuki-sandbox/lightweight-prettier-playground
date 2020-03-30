4db9a5660299ef30b24a8bd04c84d9cf1f86854d
Sosuke Suzuki <aosukeke@gmail.com>
2020-03-30 17:31:42 +0900

    Fix build config

--- scripts/bundles.js
+++ scripts/bundles.js
@@ -14,7 +14,7 @@ const plugins = [
       to: path.resolve(distPath, "index.html"),
     },
   ]),
-  new WorkerPlugin(),
+  new WorkerPlugin({ globalObject: 'self' }),
 ];
 
 const baseConfig = {
@@ -85,6 +85,7 @@ const modernConfig = Object.assign({}, baseConfig, {
   output: {
     path: distPath,
     filename: "main.mjs",
+    chunkFilename: '[name].[id].[contenthash].js',
   },
   module: {
     rules: configureLoaders({ isModern: true }),
@@ -98,6 +99,7 @@ const legacyConfig = Object.assign({}, baseConfig, {
   output: {
     path: distPath,
     filename: "main.es5.js",
+    chunkFilename: '[name].[id].[contenthash].es5.js',
   },
   module: {
     rules: configureLoaders({ isModern: false }),
